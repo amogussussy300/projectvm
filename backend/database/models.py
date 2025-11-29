fe936f3a933891567d8cb5bf0aff884fb6e31145
AlekseyElchaninov <156007377+AlekseyElcha@users.noreply.github.com>
2025-11-29 22:49:30 +0300

    Update models.py

--- backend/database/models.py
+++ backend/database/models.py
@@ -4,22 +4,26 @@ from database import Base
 class ComponentBase(Base):
     __abstract__ = True
     name: Mapped[str] = mapped_column(primary_key=True)
-    consumption: Mapped[str] = mapped_column()
 
 class CPU(ComponentBase):
     __tablename__ = "cpus"
+    consumption: Mapped[str] = mapped_column()
 
 class GPU(ComponentBase):
     __tablename__ = "gpus"
+    consumption: Mapped[str] = mapped_column()
 
 class PSU(ComponentBase):
     __tablename__ = "psus"
+    wattage: Mapped[str] = mapped_column()
 
 class RAM(ComponentBase):
     __tablename__ = "ram"
+    consumption: Mapped[str] = mapped_column()
 
 class Storage(ComponentBase):
     __tablename__ = "storages"
+    consumption: Mapped[str] = mapped_column()
 
 class Cooling(ComponentBase):
     __tablename__ = "cooling"
